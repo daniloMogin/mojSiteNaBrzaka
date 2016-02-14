35ccff122fc1eda7d74db1575712b02e5149494e
Danilo Mogin <danilo.mogin@gmail.com>
2016-02-14 20:10:05 +0100

    Sitne ispravke

--- html_ja/assets/scripts/script.js
+++ html_ja/assets/scripts/script.js
@@ -54,7 +54,7 @@ $("document").ready(function ($) {
     /*----------  slider text animations
     ------------------------------------------------------------------------------*/
     $(".anim-slider").animateSlider( {
-        autoplay	            : false,
+        autoplay	            : true,
         interval	            : 8000,
         animations 	            : 
         {
@@ -283,58 +283,6 @@ $("document").ready(function ($) {
             scrollTop: 0,
         }, scroll_top_duration
                               );
-    });
-
-    /*----------  contact form validation
-    ------------------------------------------------------------------------------*/
-    $('#contact').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-                required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "come on, you have a name don't you?",
-                minlength: "your name must consist of at least 2 characters"
-            },
-            email: {
-                required: "no email, no message",
-                email: "Your email address must be in the format of name@domain.com"
-            },
-            message: {
-                required: "um...yea, you have to write something to send this form.",
-                minlength: "thats all? really?"
-            }
-        },
-        submitHandler: function(form) {
-            $(form).ajaxSubmit({
-                type:"POST",
-                data: $(form).serialize(),
-                url:"process.php",
-                success: function() {
-                    $('#contact :input').attr('disabled', 'disabled');
-                    $('#contact').fadeTo( "slow", 0.15, function() {
-                        $(this).find(':input').attr('disabled', 'disabled');
-                        $(this).find('label').css('cursor','default');
-                        $('#success').fadeIn();
-                    });
-                },
-                error: function() {
-                    $('#contact').fadeTo( "slow", 0.15, function() {
-                        $('#error').fadeIn();
-                    });
-                }
-            });
-        }
     });
 
     /*----------  animate content in viewport
